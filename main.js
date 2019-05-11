fd4f333307b3d25fec9a685ce5b77e8d08c3dc95
patrickfinnigan <patrickfinnigan94@gmail.com>
2019-05-10 17:50:59 -0700

    so far so good

--- main.js
+++ main.js
@@ -26,7 +26,34 @@ var app4 = new Vue({
     todos: [
       { text: "Learn JavaScript" },
       { text: "Learn Vue" },
-      { text: "Build something awesome" }
+      { text: "Build something awesome" },
+      {
+        text:
+          "Add new items to this list in the console by typing in app4.todos.push({ text: 'New item' })"
+      }
     ]
+  }
+});
+
+var app5 = new Vue({
+  el: "#app-5",
+  data: {
+    message: "Hello Vue.js!"
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message
+        .split("")
+        .reverse()
+        .join("");
+      // "this" in this line of code refers to this functions OWN property of "message", and grabs it for use on the following functions
+    }
+  }
+});
+
+var app6 = new Vue({
+  el: "#app-6",
+  data: {
+    message: "Hello Vue! v-model makes whatever is placed in the input box and makes it the message property for this app! Whatever you type in this box will update the app state of message!"
   }
 });
